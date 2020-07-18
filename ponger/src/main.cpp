329b6d56c06106f28cfe2ae39a687ab54fddb24e
Jens Balvig <jens@balvig.com>
2020-07-18 13:38:42 +0900

    can now simplify ponger

--- ponger/src/main.cpp
+++ ponger/src/main.cpp
@@ -1,14 +1,7 @@
 #include <Bricks.Brick.h>
 #include <Bricks.Inbox.h>
-#include <Bricks.Outbox.h>
 #include <Bricks.PongAction.h>
 using namespace Bricks;
-
-// Custom Actions
-void debug(const uint8_t *macAddr, const Message message) {
-  Serial.print("ping value: ");
-  Serial.println(message.value);
-}
 
 void setup() {
   // Logger and ESPNOW
@@ -17,7 +10,6 @@ void setup() {
   // Enable receiving messages
   gInbox.init();
   gInbox.actions[0] = new PongAction("ponger");
-  gInbox.actions[1] = new Action("ping", &debug);
 }
 
 void loop() {
