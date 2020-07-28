87c0db99ed971b986b45489fa17b7eca035e0434
Jens Balvig <jens@balvig.com>
2020-07-28 21:24:30 +0900

    basic test for storing gateway MAC

--- test/inbox/test_inbox.cpp
+++ test/inbox/test_inbox.cpp
@@ -3,6 +3,7 @@
 #include <unity.h>
 #include <Bricks.Inbox.h>
 #include <Bricks.PongAction.h>
+#include <Bricks.StoreGatewayAction.h>
 using namespace Bricks;
 
 void setUp(void) {
@@ -10,13 +11,22 @@ void setUp(void) {
   Log.begin(LOG_LEVEL_TRACE, &Serial);
 }
 
-void test_process() {
+void test_pong() {
   const uint8_t macAddr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   Message message;
   strcpy(message.key, "ping");
 
   gInbox.actions[0] = new PongAction();
-  // gInbox.process(macAddr, message); guru meditation error in tests at the moment
+  gInbox.process(macAddr, message);
+}
+
+void test_store_gateway() {
+  const uint8_t macAddr[] = {0x98, 0xF4, 0xAB, 0x6C, 0xE6, 0xB5};
+  Message message;
+  strcpy(message.key, "ping");
+
+  gInbox.actions[0] = new StoreGatewayAction();
+  gInbox.process(macAddr, message);
 }
 
 void test_list_actions() {
@@ -29,7 +39,8 @@ void setup() {
   delay(2000); // if board doesn't support software reset via Serial.DTR/RTS
 
   UNITY_BEGIN();
-  RUN_TEST(test_process);
+  RUN_TEST(test_pong);
+  RUN_TEST(test_store_gateway);
   RUN_TEST(test_list_actions);
   UNITY_END();
 }
