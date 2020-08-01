1cc213e2d743b7071aab648621e086640a1881d3
Jens Balvig <jens@balvig.com>
2020-08-01 15:58:48 +0900

    hacking together scrollable logs

--- examples/gateway/lib/M5Logger/M5Logger.cpp
+++ examples/gateway/lib/M5Logger/M5Logger.cpp
@@ -2,9 +2,31 @@
 
 void M5Logger::init(int level) {
   M5.begin();
-  M5.Lcd.setTextWrap(true, true);
-  Log.begin(level, &M5.Lcd, false);
-  Log.setPrefix(M5Logger::scrollScreen);
+  // M5.Lcd.setTextWrap(true, true);
+
+  M5.begin();
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  //固定エリアの描画
+  M5.Lcd.setTextDatum(MC_DATUM);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLUE);
+
+  //上部固定エリア
+  M5.Lcd.fillRect(0, 0, SCREEN_WIDTH, TOP_FIXED_HEIGHT, TFT_BLUE);
+  M5.Lcd.drawString("TOP FIXED AREA", SCREEN_WIDTH/2, TOP_FIXED_HEIGHT/2);
+
+  //下部固定エリア
+  M5.Lcd.fillRect(0, SCREEN_HEIGHT-BOTTOM_FIXED_HEIGHT, SCREEN_WIDTH, BOTTOM_FIXED_HEIGHT, TFT_BLUE);
+  M5.Lcd.drawString("BOTTOM FIXED AREA", SCREEN_WIDTH/2, SCREEN_HEIGHT - BOTTOM_FIXED_HEIGHT/2);
+
+  M5.Lcd.setTextDatum(TL_DATUM);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  //スクロールクラスの初期化
+
+  // Log.begin(level, &M5.Lcd, false);
+  Log.begin(level, &stw, false);
+  // Log.setPrefix(M5Logger::scrollScreen);
 }
 
 void M5Logger::scrollScreen(Print* _logOutput) {
--- examples/gateway/lib/M5Logger/M5Logger.h
+++ examples/gateway/lib/M5Logger/M5Logger.h
@@ -3,10 +3,15 @@
 
 #include <ArduinoLog.h>
 #include <M5Stack.h>
+#include <ScrollTextWindow.h>
+
+#define TOP_FIXED_HEIGHT        24
+#define BOTTOM_FIXED_HEIGHT     16
 
 class M5Logger {
   public:
-    static void init(int level);
+    static void init(int level = LOG_LEVEL_NOTICE);
+		static ScrollTextWindow stw(TOP_FIXED_HEIGHT, BOTTOM_FIXED_HEIGHT);
   private:
     static void scrollScreen(Print* _logOutput);
 };
