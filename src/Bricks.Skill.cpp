2260f55c9c68e26d0cee63de6ca79ac98e2351a9
Jens Balvig <jens@balvig.com>
2020-09-15 12:50:46 +0900

    Richer ack responses

--- src/Bricks.Skill.cpp
+++ src/Bricks.Skill.cpp
@@ -11,10 +11,22 @@ namespace Bricks {
   }
 
   void Skill::callback(const uint8_t *macAddr, const Message message) {
+    setAckValue(message.value);
+    ack(macAddr);
     customCallback(macAddr, message);
   }
 
   bool Skill::respondsTo(const char *compareKey) {
     return (strcmp(key, compareKey) == 0 || strcmp(key, ANY) == 0);
+  }
+
+  void Skill:setAckValue(const char *value) {
+    this->ackValue = value;
+  }
+
+  void Skill:ack(const uint8_t *macAddr) {
+    char ackKey[KEY_SIZE];
+    sprintf(ackKey, BRICKS_ACK_PREFIX "%s", message.key);
+    gOutbox.send(macAddr, ackKey, ackValue);
   }
 }
--- src/Bricks.Skill.h
+++ src/Bricks.Skill.h
@@ -8,6 +8,7 @@
 namespace Bricks {
   class Skill {
     const char *ANY = "*";
+    const char *ACK_PREFIX = "ack:"
 
     public:
       Skill(const char *key = "", std::function<void(const uint8_t *macAddr, const Message message)> customCallback = nullptr);
@@ -16,6 +17,9 @@ namespace Bricks {
       virtual void callback(const uint8_t *macAddr, const Message message);
       const char *key;
     private:
+      virtual void setAckValue(const char *value);
+      void ack();
+      const char *ackValue;
       std::function<void(const uint8_t *macAddr, const Message message)> customCallback;
   };
 }
