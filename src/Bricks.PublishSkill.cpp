cf9f37f4596fe5355b8791a3accf43bff1f373f7
Jens Balvig <jens@balvig.com>
2020-09-17 08:17:57 +0900

    debug gateway crashes

--- src/Bricks.PublishSkill.cpp
+++ src/Bricks.PublishSkill.cpp
@@ -2,6 +2,7 @@
 
 namespace Bricks {
   PublishSkill::PublishSkill() : Skill("*") {
+    gEvents.publish(BRICKS_MESSAGES_IN "/gateway/awake");
   }
 
   void PublishSkill::callback(const uint8_t *macAddr, const Message message) {
