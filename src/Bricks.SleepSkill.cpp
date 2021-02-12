2a37bb82467bb5cbd16058f0b72b9b3cd0ea01fd
Jens Balvig <jens@balvig.com>
2021-02-12 10:42:57 +0900

    try going back to no cycles

--- src/Bricks.SleepSkill.cpp
+++ src/Bricks.SleepSkill.cpp
@@ -1,34 +1,24 @@
 #include <Bricks.SleepSkill.h>
 
 namespace Bricks {
-#ifdef ESP32
-  RTC_DATA_ATTR int16_t rtcSleepTime = 0;
-#endif
-
   SleepSkill::SleepSkill(const char *name) : Skill("sleep") {
     this->name = name;
     sendAwakeMessage();
-    readSleepTime();
   }
 
   void SleepSkill::callback(BRICKS_CALLBACK_SIGNATURE) {
     this->sleepTime = atoi(message.value);
-    writeSleepTime();
 
     if(validSleepTime()) {
-      sprintf(response, "Starting sleep cycles");
+      sprintf(response, "Going to sleep (Time: %d)", sleepTime);
     }
     else {
-      sprintf(response, "Stopping sleep (Rec: %d, Max: %d)", sleepTime, MAX_SLEEP_TIME);
+      sprintf(response, "Invalid sleep time (Rec: %d, Max: %d)", sleepTime, MAX_SLEEP_TIME);
     }
   }
 
   void SleepSkill::loop() {
-    if(awakeTimeout()) {
-      char message[50];
-      sprintf(message, "Going to sleep (Time: %d)", sleepTime);
-      gOutbox.send("sleeping", message);
-
+    if(validSleepTime()) {
       deepSleep();
     }
   }
@@ -46,28 +36,7 @@ namespace Bricks {
     Utils::deepSleep(sleepTime);
   }
 
-  bool SleepSkill::awakeTimeout() {
-    return validSleepTime() && millis() >= TIMEOUT;
-  }
-
   bool SleepSkill::validSleepTime() {
     return sleepTime >= 0 && sleepTime <= MAX_SLEEP_TIME;
-  }
-
-  void SleepSkill::readSleepTime() {
-#ifdef ESP8266
-    system_rtc_mem_read(RTC_SLEEP_TIME_REGISTER, &sleepTime, sizeof(sleepTime));
-#elif ESP32
-    sleepTime = Bricks::rtcSleepTime;
-#endif
-    Log.trace("SLEE: Sleep time read: %d" CR, sleepTime);
-  }
-
-  void SleepSkill::writeSleepTime() {
-#ifdef ESP8266
-    system_rtc_mem_write(RTC_SLEEP_TIME_REGISTER, &sleepTime, sizeof(sleepTime));
-#elif ESP32
-    Bricks::rtcSleepTime = sleepTime;
-#endif
   }
 }
