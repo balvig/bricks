4b35140176509aad39befc9e6e137a68ecc7b96b
Jens Balvig <jens@balvig.com>
2021-04-13 21:25:00 +0900

    try going back to no cycles (#41)

--- include/Bricks.SleepSkill.h
+++ include/Bricks.SleepSkill.h
@@ -3,21 +3,12 @@
 
 #include <ArduinoLog.h>
 
-#ifdef ESP8266
-// For ESP8266 RTC memory
-extern "C" {
-#include "user_interface.h"
-}
-#endif
-
 #include <Bricks.Skill.h>
 #include <Bricks.Constants.h>
 #include <Bricks.Utils.h>
 
 namespace Bricks {
   class SleepSkill : public Skill {
-    const uint32_t TIMEOUT = 2000;
-    const uint32_t RTC_SLEEP_TIME_REGISTER = 65;
     const uint16_t MAX_SLEEP_TIME = 10800; // seconds = 3 hours
 
     public:
@@ -29,8 +20,6 @@ namespace Bricks {
       void deepSleep();
       bool awakeTimeout();
       bool validSleepTime();
-      void readSleepTime();
-      void writeSleepTime();
       const char *name;
       int16_t sleepTime = -1;
   };
